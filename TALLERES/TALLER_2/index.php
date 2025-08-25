ca7cac565604010863e35c925ef6085aa0537a7a
sharpy26 <sharline.payne@utp.ac.pa>
2025-08-25 18:50:08 -0500

    Definidas variables de diferentes tipos

--- TALLERES/TALLER_2/index.php
+++ TALLERES/TALLER_2/index.php
@@ -1,4 +1,12 @@
 <?php
 // Tu código PHP irá aquí
+$nombre = "Sharline";
+$edad = 22;
+$altura = 1.65;
+$esEstudiante = true;
 
+echo "Nombre: $nombre<br>";
+echo "Edad: $edad<br>";
+echo "Altura: $altura<br>";
+echo "¿Es estudiante? " . ($esEstudiante ? "Sí" : "No");
 ?>
